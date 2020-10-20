453ccf46264f15556490fbed356579b8c19adb73
Hiroyuki Abe <vanica.zuey@gmail.com>
2020-10-20 23:17:28 +0900

    XSS対策修正、文字リテラル、内部生成データのh関数削除

--- www/view/admin_view.php
+++ www/view/admin_view.php
@@ -45,7 +45,7 @@
       </div>
       
       <input type="submit" value="商品追加" class="btn btn-primary">
-      <input type="hidden" name="token" value="<?php print h($token); ?>">
+      <input type="hidden" name="token" value="<?php print $token; ?>">
     </form>
 
 
@@ -82,17 +82,17 @@
               <form method="post" action="admin_change_status.php" class="operation">
                 <?php if(is_open($item) === true){ ?>
                   <input type="submit" value="公開 → 非公開" class="btn btn-secondary">
-                  <input type="hidden" name="changes_to" value="<?php h('close'); ?>">
+                  <input type="hidden" name="changes_to" value="close">
                 <?php } else { ?>
                   <input type="submit" value="非公開 → 公開" class="btn btn-secondary">
-                  <input type="hidden" name="changes_to" value="<?php h('open'); ?>">
+                  <input type="hidden" name="changes_to" value="open">
                 <?php } ?>
-                <input type="hidden" name="item_id" value="<?php print h($item['item_id']); ?>">
+                <input type="hidden" name="item_id" value="<?php print $item['item_id']; ?>">
               </form>
 
               <form method="post" action="admin_delete_item.php">
                 <input type="submit" value="削除" class="btn btn-danger delete">
-                <input type="hidden" name="item_id" value="<?php print h($item['item_id']); ?>">
+                <input type="hidden" name="item_id" value="<?php print $item['item_id']; ?>">
               </form>
 
             </td>
